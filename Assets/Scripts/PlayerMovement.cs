79f5011ca4a5ee6030fb454ad2852552629d220e
Simona <sima.simute1999@gmail.com>
2024-09-17 19:26:12 +0300

    Enemy
    Bouncing enemy
    Rotating enemy

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,14 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5.0f;
 
+    Vector3 startPos;
+
+    private void Start() 
+    {
+        startPos = transform.position;
+    }
+
+
     void Update()
     {
         //var - select the type of variable
@@ -14,5 +22,14 @@ public class PlayerMovement : MonoBehaviour
 
         //normalized - move equally in all directions
         transform.position += new Vector3(x, 0, z).normalized * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            print("Game Over");
+            transform.position = startPos;
+        }
     }
 }
