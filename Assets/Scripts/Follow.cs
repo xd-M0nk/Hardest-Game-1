427cfc8549bf04e7f8479b11135203b326101ad3
Simona <sima.simute1999@gmail.com>
2024-09-22 15:25:43 +0300

    Enemies

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        
+    }
+}
